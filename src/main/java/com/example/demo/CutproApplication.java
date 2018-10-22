af73849562e5d71d7878b8cff8c60633e4de3a9b
msreddy1993 <santhosh.mogulla@gmail.com>
2018-10-23 01:15:16 +0530

    added base src files

---
+++ src/main/java/com/example/demo/CutproApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutproApplication.class, args);
+	}
+}
